346840d5322da63a8f5ab97045a5ea6a4722832b
caleb <shawcaleb1688@gmail.com>
2023-12-08 23:38:31 -0500

    server and router are working

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,9 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig')
+
+async function getProjectById(project_id) {
+
+}
+
+
+module.exports = { getProjectById }
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,21 @@
 // build your `/api/projects` router here
+const router = require('express').Router()
+const Project = require('./model')
+
+router.get('/', (req, res, next) => {
+   const message =  res.json('its working')
+   return message;
+})
+
+
+
+
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+        customMessage: 'something went wrong inside the project router',
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
+module.exports = router
